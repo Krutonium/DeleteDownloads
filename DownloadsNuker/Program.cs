0737c011f624af38a3b5ad742c8177f113c3f7e2
Krutonium <PFCKrutonium@gmail.com>
2020-12-01 10:21:13 -0500

    Full Program Dawg

---
+++ DownloadsNuker/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using SearchOption = System.IO.SearchOption;
+
+namespace DownloadsNuker
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Evaluating Downloads Folder...");
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "/Downloads";
+            Console.WriteLine(path);
+            string[] fileList = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            long size=0;
+            foreach (var file in fileList)
+            {
+                FileInfo fileInfo = new  FileInfo(file);
+                size += fileInfo.Length;
+            }
+            size = size / 1024 / 1024 / 1024;
+            Console.WriteLine("Downloads Folder is " + size + "GB");
+            if (size > 200)
+            {
+                Console.WriteLine("Would you like to delete your Downloads Directory?");
+                Console.WriteLine("[y/N]");
+                var userKey = Console.ReadKey();
+                if (userKey.Key == ConsoleKey.Y)
+                {
+                    foreach (var file in fileList)
+                    {
+                        Console.WriteLine("Deleting " + file);
+                        File.Delete(file);
+                    }
+                    Console.WriteLine("Process Complete!");
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Will prompt at next launch.");
+                    Console.Read();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your downloads folder is less than 200GB! Congrats!");
+                System.Threading.Thread.Sleep(3000);
+            }
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# DeleteDownloads
+
+I wrote this program because I have a problem where I don't delete downloads.
+This tool should be set up to autorun with Windows or Linux (And maybe MacOS?), and it will calculate the size of your Downloads folder, and if it determines that it is bigger than 200GB, it will offer to delete it for you.
+
+# WARNING
+## I AM NOT RESPONSIBLE FOR LOST DATA AS A RESULT OF USING THIS SOFTWARE
+## YOU USE IT AT YOUR OWN RISK.
